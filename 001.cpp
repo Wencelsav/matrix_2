d46218aee5abb0f4025f846f30dbf3778c6b80f5
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2018-03-05 19:45:59 +0300

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -1,204 +1,186 @@
-
+#include "stdafx.h"
 #include <iostream>
 #include <sstream>
 #include <fstream>
-
+#include <assert.h>
 using namespace std;
 
-void destroy(int ** elements,
-	unsigned int rows)
-{
-	for (unsigned int i = 0; i < rows; ++i) {
-		delete[] elements[i];
-	}
-	delete[] elements;
-}
-void finit() {
-	ofstream fout;
-	fout.open("A.txt");
-	fout << "3, 3\n2 2 2\n2 2 2\n2 2 2";
-	fout.close();
-
-	fout.open("B.txt");
-	fout << "2, 2\n1 1 1\n1 1 1\n1 1 1";
-	fout.close();
-
-	fout.open("C.txt");
-	fout << "3, 3\n1 2 2\n0 4 4\n0 4 0";
-	fout.close();
-
-	fout.open("D.txt");
-	fout << "3, 3\n1 2 3\n4 5 6\n7 8 9";
-	fout.close();
-
-}
-int** create_matrix(unsigned int columns,
-	unsigned int rows)
-{
-	int** matrix;
-	matrix = new int*[rows];
-	for (unsigned int i = 0; i < rows; ++i) {
-		matrix[i] = new int[columns];
-		for (unsigned int j = 0; j < columns; ++j) {
-			matrix[i][j] = 0.0f;
-		}
-	}
-	return matrix;
-}
 class matrix_t {
-	int ** data;
+	int **data;
 	unsigned int rows;
-	unsigned int collumns;
+	unsigned int columns;
 public:
-	bool isnull() {
-		if (data == nullptr) {
-			return true;
+	~matrix_t() {
+		for (unsigned int i = 0; i < rows; i++) {
+			delete[] data[i];
 		}
-		return false;
+		delete[] data;
 	}
-	  matrix_t add(matrix_t & other) {
-		matrix_t result;
-		
-		if (collumns != other.collumns || rows != other.rows) {
-			result.data = nullptr;
-			destroy(data, rows);
-			destroy(other.data, other.rows);
-			return result;
-		}
-		result.data = create_matrix(rows, collumns);
-		result.rows = rows;
-		result.collumns = collumns;
-		for (unsigned int j = 0; j < rows; j++) {
-			for (unsigned int i = 0; i < collumns; i++) {
-				result.data[j][i] = data[j][i] + other.data[j][i];
+
+	matrix_t(unsigned int rows = 0, unsigned int columns = 0) {
+		this->rows = rows;
+		this->columns = columns;
+	}
+
+	matrix_t(const matrix_t & object) {
+		rows = object.rows;
+		columns = object.columns;
+		data = new int *[rows];
+		for (unsigned int i = 0; i < rows; ++i) {
+			data[i] = new int[columns];
+			for (unsigned int j = 0; j < columns; ++j) {
+				data[i][j] = object.data[i][j];
 			}
 		}
-	  	
-		return result;
- 	};
-	  void clearmem() {
-		  destroy(data, rows);
 	}
-	  matrix_t sub(matrix_t & other) {
-		  matrix_t result;
 
-		  if (collumns != other.collumns || rows != other.rows) {
-			  result.data = nullptr;
-			  destroy(data, rows);
-			  destroy(other.data, other.rows);
-			  return result;
-		  }
-		  result.data = create_matrix(rows, collumns);
-		  result.rows = rows;
-		  result.collumns = collumns;
-		  for (unsigned int j = 0; j < rows; j++) {
-			  for (unsigned int i = 0; i < collumns; i++) {
-				  result.data[j][i] = data[j][i] - other.data[j][i];
-			  }
-		  }
+	matrix_t add(matrix_t & other) {
+		if (!(rows == other.rows && columns == other.columns))
+		{
+			cout << "An error has occured while reading input data.\n";
+			exit(0);
+		}
 
-		  return result;
+		matrix_t res(rows, columns);
+		res.data = new int *[rows];
+		for (unsigned int i = 0; i < rows; ++i) {
+			res.data[i] = new int[columns];
+			for (unsigned int j = 0; j < columns; ++j) {
+				res.data[i][j] = data[i][j] + other.data[i][j];
+			}
+		}
+		return res;
 	}
-	  matrix_t mul(matrix_t & other) {
-		  matrix_t result;
-		  if (collumns != other.rows) {
-			  result.data = nullptr;
-			  destroy(data, rows);
-			  destroy(other.data, other.rows);
-			  return result;
-			  return result;
-		  }
-		  result.data = create_matrix(other.collumns, rows);
-		  for (unsigned int j = 0; j < rows; j++) {
-			  for (unsigned int i = 0; i < other.collumns; i++) {
-				  float y = 0;
-				  for (unsigned int z = 0; z < collumns; z++) {
-					  y += data[j][z] * other.data[z][i];
-				  }
-				  result.data[j][i] = y;
-			  }
-		  }
-		  result.collumns = other.collumns;
-		  result.rows = rows;
-		  return result;
+	matrix_t sub(matrix_t & other) {
+		if (!(rows == other.rows && columns == other.columns))
+		{
+			cout << "An error has occured while reading input data.\n";
+			exit(0);
+		}
 
+		matrix_t res(rows, columns);
+		res.data = new int *[rows];
+		for (unsigned int i = 0; i < rows; ++i) {
+			res.data[i] = new int[columns];
+			for (unsigned int j = 0; j < columns; ++j) {
+				res.data[i][j] = data[i][j] - other.data[i][j];
+			}
+		}
+		return res;
 	}
-	  matrix_t trans() {
-		  matrix_t result;
-		  result.data= create_matrix(rows, collumns);
+	matrix_t mul(matrix_t & other) {
+		if (columns != other.rows)
+		{
+			cout << "An error has occured while reading input data.\n";
+			exit(0);
+		}
 
-		  for (unsigned int j = 0; j < collumns; j++) {
-			  for (unsigned int i = 0; i < rows; i++) {
-				  result.data[j][i] = data[i][j];
-			  }
-		  }
-		  result.collumns= rows;
-		  result.rows = collumns;
-		  return result;
+		matrix_t res(rows, other.columns);
+		res.data = new int *[rows];
+		for (unsigned int i = 0; i < rows; ++i) {
+			res.data[i] = new int[columns];
+			for (unsigned int j = 0; j < other.columns; ++j) {
+				res.data[i][j] = 0;
+				for (unsigned int k = 0; k < columns; k++)
+					res.data[i][j] += (data[i][k] * other.data[k][j]);
+			}
+		}
+		return res;
+	}
+	matrix_t trans() {
+		matrix_t res(columns, rows);
+		res.data = new int *[columns];
+		for (unsigned int i = 0; i < columns; ++i) {
+			res.data[i] = new int[rows];
+			for (unsigned int j = 0; j < rows; ++j) {
+				res.data[i][j] = data[j][i];
+			}
+		}
+		return res;
 	}
 
 	ifstream & read(ifstream & stream) {
-		string header;
+		string line, str;
+		getline(stream, line);
+		unsigned int rows, columns;
+		char symbol;
 
-		char razdel;
-		getline(stream, header);
-		istringstream str(header);
-		if (!((str >> rows) && (str >> razdel) && (str >> collumns) && (razdel == ','))) {
-			stream.setstate(std::ios::failbit);
+		if (!(stream >> rows && stream >> symbol && symbol == ',' && stream >> columns))
+		{
+			stream.setstate(ios::failbit);
+			return stream;
 		}
-		
-		data = create_matrix(collumns,rows);
-		for (unsigned int j = 0; j < rows; j++) {
-			string new_row;
-			getline(stream, new_row);
-			
-			istringstream sstream(new_row);
-			for (unsigned int i = 0; i < collumns; i++) {
-				if (!(sstream >> data[j][i])) {
-					destroy(data, rows);
-					stream.setstate(std::ios::failbit);
-					break;
+		this->rows = rows;
+		this->columns = columns;
+
+		int ** elements = new int *[rows];
+		for (unsigned int i = 0; i < rows; ++i) {
+			elements[i] = new int[columns];
+			getline(stream, str);
+			for (unsigned int j = 0; j < columns; ++j) {
+				if (!(stream >> elements[i][j])) {
+					stream.setstate(ios::failbit);
+					return stream;
 				}
-
 			}
 		}
-		
+
+		data = new int *[rows];
+		for (unsigned int i = 0; i < rows; ++i) {
+			data[i] = new int[columns];
+			for (unsigned int j = 0; j < columns; ++j) {
+				data[i][j] = elements[i][j];
+			}
+		}
+
+		for (unsigned int i = 0; i < rows; ++i) {
+			delete[] elements[i];
+		}
+		delete[] elements;
 		return stream;
-	};
+	}
 	ostream & write(ostream & stream) {
-		for (unsigned int j = 0; j < rows; j++) {
-			for (unsigned int i = 0; i < collumns; i++) {
-				
-				
-				
- 				stream << data[j][i] << "\t";
+		stream << endl;
+		for (unsigned int i = 0; i < rows; ++i) {
+			for (unsigned int j = 0; j < columns; ++j) {
+				stream << data[i][j] << ' ';
 			}
-			stream << "\n";
+			stream << endl;
 		}
 		return stream;
-	};
-
+	}
 };
-bool getcommandifile(ifstream& fs1, ifstream& fs2, char &op) {
+
+bool read(istream & stream, matrix_t & matrix)
+{
+	string file_name;
+	if (stream >> file_name) {
+		ifstream fin;
+		fin.open(file_name);
+		if ((fin.is_open()) && (matrix.read(fin)))
+			return true;
+	}
+	return false;
+}
+
+
+bool getcommandifile(ifstream & fs1, ifstream & fs2, char & op) {
 	op = 'q';
 	string fn;
 	getline(cin, fn);
-
-
-
 
 	istringstream sfn(fn);
 	string name1 = "";
 	string name2 = "";
 	char hop;
 	while (sfn >> hop) {
-		if (hop != '+'&&hop != '-'&&hop != 'T'&&hop != '*') {
+		if (hop != '+' && hop != '-' && hop != 'T' && hop != '*') {
 			name1 += hop;
 		}
 		if (hop == '+' || hop == '-' || hop == '*') {
 			op = hop;
 			while (sfn >> hop) {
-				if (hop != '+'&&hop != '-'&&hop != 'T'&&hop != 'R'&&hop != '*') {
+				if (hop != '+' && hop != '-' && hop != 'T' && hop != '*') {
 					name2 += hop;
 				}
 				else {
@@ -207,12 +189,13 @@ bool getcommandifile(ifstream& fs1, ifstream& fs2, char &op) {
 			}
 			break;
 		}
-		if (hop == 'T' ) {
+		if (hop == 'T') {
 			op = hop;
 			break;
 		}
-
-
+	}
+	if (op != '+' && op != '-' && op != 'T' && op != '*') {
+		return false;
 	}
 	if (name1 != "") {
 		fs1.open(name1);
@@ -223,27 +206,46 @@ bool getcommandifile(ifstream& fs1, ifstream& fs2, char &op) {
 	if (fs1.is_open() && (op == 'T')) {
 		return true;
 	}
-	else if (fs1.is_open() && fs2.is_open() && op != 'q') {
+	else if (fs1.is_open() && fs2.is_open()) {
 		return true;
 	}
 	else {
 		return false;
 	}
 }
-
-
-int main()
+void create_files()
 {
-	char com;
-	finit();
-	ifstream mtr1,mtr2;
-	matrix_t matrix1, matrix2, matrix3;
-	if (!getcommandifile(mtr1, mtr2, com)) {
+	ofstream fout;
+	fout.open("A.txt");
+	fout << "A.txt\n3, 3\n2 2 2\n2 2 2\n2 2 2";
+	fout.close();
 
+	fout.open("B.txt");
+	fout << "B.txt\n3, 3\n1 1 1\n1 1 1\n1 1 1";
+	fout.close();
+
+	fout.open("C.txt");
+	fout << "C.txt\n3, 3\n1 2 3\n4 5 6\n7 8 9";
+	fout.close();
+
+	fout.open("D.txt");
+	fout << "D.txt\n3, 2\n1 2\n4 5\n7 8";
+	fout.close();
+}
+
+
+int main() {
+	char com;
+	create_files();
+	ifstream mtr1, mtr2;
+	matrix_t matrix1, matrix2;
+	if (!getcommandifile(mtr1, mtr2, com)) {
+		mtr1.close();
+		mtr2.close();
 		cout << "An error has occured while reading input data";
 		exit(0);
 	}
-	
+
 	if (!matrix1.read(mtr1)) {
 		cout << "An error has occured while reading input data";
 		mtr1.close();
@@ -251,57 +253,42 @@ int main()
 		exit(0);
 	}
 	if (com == 'T') {
-		matrix3 = matrix1.trans();
+		matrix_t matrix3 = matrix1.trans();
 		matrix3.write(cout);
-		matrix1.clearmem();
-		matrix3.clearmem();
 		mtr1.close();
 		mtr2.close();
-		
-		exit(0);
 
+		exit(0);
 	}
-	else if (com != '+'&&com != '-'&&com != '*') {
+	else if (com != '+' && com != '-' && com != '*') {
 		cout << "An error has occured while reading input data";
-		matrix1.clearmem();
 		mtr1.close();
 		mtr2.close();
 		exit(0);
 	}
 	if (!matrix2.read(mtr2)) {
 		cout << "An error has occured while reading input data";
-		matrix1.clearmem();
 		mtr1.close();
 		mtr2.close();
 		exit(0);
 	}
-	switch (com) {
-	case '+':
-		matrix3 = matrix1.add(matrix2);
-		break;
-	case '-':
-		matrix3 = matrix1.sub(matrix2);
-		break;
-	case '*':
-		matrix3 = matrix1.mul(matrix2);
-		break;
-	}
-	if (!matrix3.isnull()) {
+
+	if (com == '+') {
+		matrix_t matrix3 = matrix1.add(matrix2);
 		matrix3.write(cout);
-		matrix1.clearmem();
-		matrix2.clearmem();
-		matrix3.clearmem();
-		mtr1.close();
-		mtr2.close();
 	}
-	else {
-		cout << "Wrong matrixes";
-		mtr1.close();
-		mtr2.close();
-
+	else if (com == '-') {
+		matrix_t matrix3 = matrix1.sub(matrix2);
+		matrix3.write(cout);
+	}
+	else if (com == '*') {
+		matrix_t matrix3 = matrix1.mul(matrix2);
+		matrix3.write(cout);
 	}
 
-	
-    return 0;
+	mtr1.close();
+	mtr2.close();
+
+	return 0;
 }
 
